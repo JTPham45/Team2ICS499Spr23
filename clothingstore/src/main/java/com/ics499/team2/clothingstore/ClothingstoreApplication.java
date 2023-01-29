4d077341e1f69d9a117e7deb72d1e068d2bc8bf7
Dylan Skokan <dylanskokan@gmail.com>
2023-01-29 11:34:59 -0600

    initial commit message

---
+++ clothingstore/src/main/java/com/ics499/team2/clothingstore/ClothingstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ics499.team2.clothingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingstoreApplication.class, args);
+	}
+
+}
---
+++ clothingstore/src/test/java/com/ics499/team2/clothingstore/ClothingstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ics499.team2.clothingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
