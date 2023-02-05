0da0192f84e03bf392d34356d06da3151ca155ac
Tom Waterman <93015416+TomWaterdude@users.noreply.github.com>
2023-02-05 09:29:18 -0600

    Initial commit - Tom

--- clothingstore/src/main/java/com/ics499/team2/clothingstore/ClothingstoreApplication.java
+++ clothingstore/src/main/java/com/ics499/team2/clothingstore/ClothingstoreApplication.java
@@ -8,7 +8,7 @@ public class ClothingstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClothingstoreApplication.class, args);
-		// Mic check is this on??? - Tom
+		// making an initial commit
 	}
 
 }
